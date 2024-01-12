590202aa772e614df2e30a1ec9bc9bd766dcae32
Shivam Patidar <125869590+PROShivamPatidar@users.noreply.github.com>
2024-01-12 17:01:47 +0000

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+// import React { useState } from "react";
 
 const App = () => {
+  let [change, setChange] = useState("");
+  function handleInput(e) {
+    setChange(e.target.value)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <label>Enter your name:</label>
+      <input type="text" onChange={handleInput}></input>
+      <p>{change}</p>
     </div>
   )
 }
